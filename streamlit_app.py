4b153cbc5444432b7b5a6e501b70d2f0fe8d4228
snowflakebykrutika <168755328+snowflakebykrutika@users.noreply.github.com>
2024-05-02 22:34:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -50,4 +49,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 st.success('Your Smoothie is ordered!', icon="✅")
 
-
+import requests
